77b680bb19c2b3a2009b62d3b03fe328c7a8e13a
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-17 22:52:44 +0200

    Added create view and create funcatiolity

--- Saraha/Controllers/MessagesController.cs
+++ Saraha/Controllers/MessagesController.cs
@@ -3,13 +3,26 @@ public class MessagesController : Controller
 {
     private readonly SarahaContext _context;
 
-    public MessagesController(SarahaContext context)
-    {
-        _context = context;
-    }
+    public MessagesController(SarahaContext context) => _context = context;
 
-    public IActionResult Index()
+    public IActionResult Index() => View(_context.Messages.ToList());
+
+    //Get the view
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Message message)
     {
-        return View(_context.Messages.ToList());
+        if (ModelState.IsValid)
+        {
+            message.Created = DateTime.Now;
+            _context.Messages.Add(message);
+            _context.SaveChanges();
+            TempData["success"] = "Message Sent Successfully";
+            return RedirectToAction(nameof(Index));
+        }
+        return View();
+
     }
 }
